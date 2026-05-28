8f6944bed8c416bd3f425b05aac4a359a8cad044
Abhijith V Ashok <abhi2349jith@gmail.com>
2026-05-28 20:15:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() { 
+
+  return (
+
+    <div className='page-shell'>
+      <Navbar />
+      <main className='container home-page'>
+        <PlayerSearch />
+        <TrendingComparisons />
+        <RecentComparisons />
+      </main>
+    </div>
+
+  );
+}
+
+export default App
